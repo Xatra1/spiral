1cd55a3845c8233d2f2956d59a6b3201705dafbc
Xatra1 <xatra169@gmail.com>
2025-11-17 00:08:02 -0500

    reduce spiral alpha factor a litle

--- main.js
+++ main.js
@@ -1,58 +1,55 @@
-const ctx = document.getElementById('canvas').getContext('2d'),
-    space = document.getElementById('space'),
-    canvas = ctx.canvas;
+const ctx = document.getElementById("canvas").getContext("2d"),
+  canvas = ctx.canvas;
 //const incrementString = document.getElementById('increment');
 
-
-var increment = 0,
-    centerX,
-    centerY,
-    width,
-    height,
-    add = 1;
+let increment = 0,
+  centerX,
+  centerY,
+  width,
+  height,
+  add = 1;
 
 setInterval(() => {
-    if (add) {
-        increment <= 9.43 ? increment += .00025 : add = 0;
-    } else {
-        increment >= -9.43 ? increment -= .00025 : add = 1;
-    }
-    //incrementString.textContent = `Increment: ${increment}`
+  if (add) {
+    increment <= 9.43 ? increment += .00025 : add = 0;
+  } else {
+    increment >= -9.43 ? increment -= .00025 : add = 1;
+  }
+  //incrementString.textContent = `Increment: ${increment}`
 }, 1);
 
 function spiral() {
-    if (canvas.width != innerWidth || canvas.height != innerHeight) {
-        canvas.width = innerWidth;
-        canvas.height = innerHeight;
-    }
+  if (canvas.width != innerWidth || canvas.height != innerHeight) {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+  }
 
-    width = canvas.width;
-    height = canvas.height;
-    centerX = width / 2;
-    centerY = height / 2;
+  width = canvas.width;
+  height = canvas.height;
+  centerX = width / 2;
+  centerY = height / 2;
+  draw(`rgba(0, 255, 0, 0.7)`, 5);
 
-    draw(`rgba(0, 255, 0, 1)`, 5);
-
-    setTimeout(() => {
-        requestAnimationFrame(spiral)
-    }, 1000 / 60);
+  setTimeout(() => {
+    requestAnimationFrame(spiral);
+  }, 1000 / 60);
 }
 
 function draw(color, num) {
-    ctx.moveTo(centerX, centerY);
-    ctx.beginPath();
+  ctx.moveTo(centerX, centerY);
+  ctx.beginPath();
 
-    ctx.fillStyle = "rgba(0, 0, 0, 1)";
-    ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = "rgba(0, 0, 0, 1)";
+  ctx.fillRect(0, 0, width, height);
 
-    for (i = 0; i < innerHeight; i++) {
-        angle = increment * i;
-        x = centerX + (num * angle) * Math.cos(angle);
-        y = centerY + (num * angle) * Math.sin(angle);
-        ctx.lineTo(x, y);
-    }
+  for (i = 0; i < innerHeight; i++) {
+    angle = increment * i;
+    x = centerX + (num * angle) * Math.cos(angle);
+    y = centerY + (num * angle) * Math.sin(angle);
+    ctx.lineTo(x, y);
+  }
 
-    ctx.strokeStyle = color;
-    ctx.stroke();
+  ctx.strokeStyle = color;
+  ctx.stroke();
 }
 spiral();
