8582bb431132362c496760d9588a68ae98b8604e
Xatra1 <xatra169@gmail.com>
2025-11-18 10:18:40 -0500

    rename keyhandler.js to be more accurate; move all js files into src/

---
+++ src/eventhandler.js
@@ -0,0 +1,72 @@
+const fsButton = document.getElementById("fsButton");
+
+let color = "0, 255, 0",
+  secret,
+  keySequence = [],
+  fullscreen = 0;
+
+document.addEventListener("keypress", (e) => {
+  keySequence.push(e.key);
+  const keyString = keySequence.join("");
+
+  if (keyString.match(/white/i) == "white") {
+    secret = "white";
+    keySequence = [];
+  } else if (keyString.match(/invert/i) == "invert") {
+    secret = "invert";
+    keySequence = [];
+  }
+
+  switch (e.key) {
+    case "1":
+      color = "255, 0, 0";
+      break;
+
+    case "2":
+      color = "255, 127, 0";
+      break;
+
+    case "3":
+      color = "255, 255, 0";
+      break;
+
+    case "4":
+      color = "0, 255, 0";
+      break;
+
+    case "5":
+      color = "0, 0, 255";
+      break;
+
+    case "6":
+      color = "0, 255, 255";
+      break;
+
+    case "7":
+      color = "255, 0, 255";
+      break;
+
+    case "8":
+      color = "255, 255, 255";
+      break;
+
+    case "9":
+      secret = "twin";
+      break;
+
+    case "0":
+      secret = "";
+      previousSecret = "";
+      break;
+  }
+});
+
+fsButton.addEventListener("click", () => {
+  if (!fullscreen) {
+    document.body.requestFullscreen();
+    fullscreen = 1;
+  } else {
+    document.exitFullscreen();
+    fullscreen = 0;
+  }
+});
---
+++ src/main.js
@@ -0,0 +1,59 @@
+const ctx = document.getElementById("canvas").getContext("2d"),
+  canvas = ctx.canvas;
+
+let increment = 0,
+  add = 1,
+  centerX,
+  centerY,
+  width,
+  height;
+
+setInterval(() => {
+  if (add) {
+    increment <= 9.43 ? increment += .00025 : add = 0;
+  } else {
+    increment >= -9.43 ? increment -= .00025 : add = 1;
+  }
+}, 1);
+
+function spiral() {
+  if (canvas.width != innerWidth || canvas.height != innerHeight) {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+  }
+
+  width = canvas.width;
+  height = canvas.height;
+  centerX = width / 2;
+  centerY = height / 2;
+
+  ctx.fillStyle = "rgba(0, 0, 0, 1)";
+  if (secret == "invert") ctx.fillStyle = "rgba(200, 200, 200, 1)";
+  ctx.fillRect(0, 0, width, height);
+
+  if (secret != "invert") draw(`rgba(${color}, 0.7`, 5);
+  if (secret == "white") draw("rgba(255, 255, 255, 0.7)", 5);
+  if (secret == "twin") draw(`rgba(${color}, 0.7`, -5);
+  if (secret == "invert") draw(`rgba(0, 0, 0, 0.7)`, 5);
+
+  setTimeout(() => {
+    requestAnimationFrame(spiral);
+  }, 1000 / 60);
+}
+
+function draw(color, num) {
+  ctx.moveTo(centerX, centerY);
+  ctx.beginPath();
+
+  for (i = 0; i < innerHeight; i++) {
+    angle = increment * i;
+    x = centerX + (num * angle) * Math.cos(angle);
+    y = centerY + (num * angle) * Math.sin(angle);
+    ctx.lineTo(x, y);
+  }
+
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+
+spiral();
